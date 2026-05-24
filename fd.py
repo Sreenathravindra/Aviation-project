3f19445516a2028146d3e73ea4f046e1127f5b8b
Sreenathravindra <sreenathravindra051@gmail.com>
2026-05-24 23:44:24 +0530

    Project uploded

--- fd.py
+++ fd.py
@@ -1,8 +1,8 @@
-def fl_list(a):
+def flat_list(a):
     flated =[]
     for items in a:
         if type(items) is list:
-            flated.extend(fl_list(items))
+            flated.extend(flat_list(items))
         else:
             flated.append(items)
     return flated
@@ -10,4 +10,4 @@ def fl_list(a):
 if __name__ == '__main__':
     a = [[0],[1, 2], [3, 4, 5], [6], 7, [8, 9], [10,[11,[12]]]]
 
-    print(fl_list(a))
+    print(flat_list(a))
